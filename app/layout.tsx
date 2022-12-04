211fbaa004507a9d7010817d23f2925a8fa0f434
Pablo Garcia <pgarciacamou@gmail.com>
2022-12-04 15:37:52 -0800

    feat: add google analytics 4

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import "../styles/globals.css";
 
 export default function RootLayout({
@@ -8,6 +9,20 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head />
+      {/* <!-- Global tag (gtag.js) - Google Analytics --> */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-M0RXHWHRXM"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-M0RXHWHRXM');
+        `}
+      </Script>
       <body>{children}</body>
     </html>
   );
