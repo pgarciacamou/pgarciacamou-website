c3906da862591c321599e225b12d62a042c326b1
Pablo Garcia <pgarciacamou@gmail.com>
2022-12-03 17:21:59 -0800

    feat: add theme color

--- app/home/home.tsx
+++ app/home/home.tsx
@@ -23,10 +23,13 @@ function PabloGarcia() {
 export default function Home() {
   return (
     <div className={styles.container}>
+      {/* <PabloGarcia /> */}
       <main className={styles.main}>
         <div className={styles.spacer} />
         <h1 className={styles.title} aria-label="Pablo Garcia">
-          {"<PabloGarcia />"}
+          <span style={{ color: "#808080" }}>{"<"}</span>
+          <span style={{ color: "#4EC9B0" }}>{"PabloGarcia "}</span>
+          <span style={{ color: "#808080" }}>{"/>"}</span>
         </h1>
         <p className={styles.description}>
           <a
@@ -38,7 +41,67 @@ export default function Home() {
           </a>
         </p>
         <pre className={[styles.code].join(" ")}>
-          <code>{descriptionCode.trim()}</code>
+          <code style={{ color: "#808080" }}>
+            <span style={{ color: "#569CD6" }}>function</span>{" "}
+            <span style={{ color: "#DCDCAA" }}>PabloGarcia</span>
+            <span>{"() {\n  "}</span>
+            <span style={{ color: "#C586C0" }}>return </span>
+            <span>{"(\n    <>"}</span>
+            <span>{"\n      <"}</span>
+            <span style={{ color: "#569CD6" }}>meta </span>
+            <span style={{ color: "#9CDCFE" }}>name</span>
+            <span style={{ color: "#D4D4D4" }}>=</span>
+            <span style={{ color: "#CE9178" }}>"author" </span>
+            <span style={{ color: "#9CDCFE" }}>content</span>
+            <span style={{ color: "#D4D4D4" }}>=</span>
+            <span style={{ color: "#CE9178" }}>"Pablo Garcia"</span>
+            <span>{" />"}</span>
+            <span>{"\n      <"}</span>
+            <span style={{ color: "#569CD6" }}>meta </span>
+            <span style={{ color: "#9CDCFE" }}>name</span>
+            <span style={{ color: "#D4D4D4" }}>=</span>
+            <span style={{ color: "#CE9178" }}>"description" </span>
+            <span style={{ color: "#9CDCFE" }}>content</span>
+            <span style={{ color: "#D4D4D4" }}>=</span>
+            <span style={{ color: "#CE9178" }}>
+              "Quality centered software engineer."
+            </span>
+            <span>{" />"}</span>
+            <span>{"\n      <"}</span>
+            <span style={{ color: "#569CD6" }}>meta </span>
+            <span style={{ color: "#9CDCFE" }}>name</span>
+            <span style={{ color: "#D4D4D4" }}>=</span>
+            <span style={{ color: "#CE9178" }}>"yoe" </span>
+            <span style={{ color: "#9CDCFE" }}>content</span>
+            <span style={{ color: "#D4D4D4" }}>=</span>
+            <span style={{ color: "#CE9178" }}>
+              "{new Date().getFullYear() - 2012}+"
+            </span>
+            <span>{" />"}</span>
+            <span>{"\n      <"}</span>
+            <span style={{ color: "#569CD6" }}>meta</span>
+            <span>{"\n        "}</span>
+            <span style={{ color: "#9CDCFE" }}>name</span>
+            <span style={{ color: "#D4D4D4" }}>=</span>
+            <span style={{ color: "#CE9178" }}>"education" </span>
+            <span>{"\n        "}</span>
+            <span style={{ color: "#9CDCFE" }}>content</span>
+            <span style={{ color: "#D4D4D4" }}>=</span>
+            <span style={{ color: "#569CD6" }}>{"{"}</span>
+            <span style={{ color: "#CE9178" }}>
+              {"`"}
+              {"\n          B.Eng. in Computer Software."}
+              {"\n          M.S. in Computer Science."}
+              {"\n          M.S. Specialization in Computing Systems."}
+              {"\n        `"}
+            </span>
+            <span style={{ color: "#569CD6" }}>{"}"}</span>
+            <span>{"\n      />"}</span>
+            <span>{"\n    </>"}</span>
+            <span>{"\n  )"}</span>
+            <span style={{ color: "#D4D4D4" }}>;</span>
+            <span>{"\n}"}</span>
+          </code>
         </pre>
       </main>
 
