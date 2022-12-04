289b8925bf4970c1660516fee6cc4b74e4457257
Pablo Garcia <pgarciacamou@gmail.com>
2022-12-04 15:55:29 -0800

    fix: load analytics at client

---
+++ app/analytics.tsx
@@ -0,0 +1,26 @@
+"use client";
+import { Analytics } from "@vercel/analytics/react";
+import Script from "next/script";
+
+export default function MyAnalytics() {
+  return (
+    <>
+      {/* <!-- Vercel Analytics - https://vercel.com/docs/concepts/analytics/audiences/quickstart --> */}
+      <Analytics />
+      {/* <!-- Global tag (gtag.js) - Google Analytics --> */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-M0RXHWHRXM"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-M0RXHWHRXM');
+        `}
+      </Script>
+    </>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
-import { Analytics } from "@vercel/analytics/react";
-import Script from "next/script";
 import "../styles/globals.css";
+import Analytics from "./analytics";
 
 export default function RootLayout({
   children,
@@ -10,23 +9,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head />
-      {/* <!-- Vercel Analytics - https://vercel.com/docs/concepts/analytics/audiences/quickstart --> */}
-      <Analytics />
-      {/* <!-- Global tag (gtag.js) - Google Analytics --> */}
-      <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-M0RXHWHRXM"
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-M0RXHWHRXM');
-        `}
-      </Script>
       <body>{children}</body>
+      <Analytics />
     </html>
   );
 }
