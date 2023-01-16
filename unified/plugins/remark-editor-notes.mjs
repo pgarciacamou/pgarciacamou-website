85b553dee5a4e21df66769348f11c3ff5e1e03ad
Pablo Garcia <pgarciacamou@gmail.com>
2023-01-15 16:58:37 -0800

    WIP

--- unified/plugins/remark-editor-notes.mjs
+++ unified/plugins/remark-editor-notes.mjs
@@ -11,6 +11,7 @@ export default function remarkEditorNotes() {
   return (tree) => {
     if (process.env.VERCEL_ENV === "production") {
       console.log("log   - editor notes removed");
+      remove(tree, { cascade: true }, hasAttributeName("editornote"));
       remove(tree, { cascade: true }, hasAttributeName("editornotes"));
     }
   };
